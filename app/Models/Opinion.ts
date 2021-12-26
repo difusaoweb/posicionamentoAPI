dc6b9454ada9ef4f3aada92a041297445d5d6b70
Wiatagan Paz <pazwiatagan@gmail.com>
2021-12-26 16:25:51 -0300

    Model Opinion alterado

--- app/Models/Opinion.ts
+++ app/Models/Opinion.ts
@@ -1,5 +1,5 @@
 import { DateTime } from 'luxon'
-import { BaseModel, column, belongsTo, BelongsTo } from '@ioc:Adonis/Lucid/Orm'
+import { BaseModel, column, belongsTo, BelongsTo, hasMany, HasMany } from '@ioc:Adonis/Lucid/Orm'
 
 import User from 'App/Models/User'
 import Affirmation from 'App/Models/Affirmation'
@@ -10,16 +10,16 @@ export default class Opinion extends BaseModel {
   public id: number
 
   @column()
-  public opinion_author: number
+  public opinionAuthor: number
 
   @column()
   public evaluation: number
 
   @column()
-  public affirmation_parent: number
+  public affirmationParent: number | null
 
   @column()
-  public opinion_parent: number
+  public opinionParent: number | null
 
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
@@ -27,12 +27,14 @@ export default class Opinion extends BaseModel {
   @column.dateTime({ autoCreate: true, autoUpdate: true })
   public updatedAt: DateTime
 
+  @hasMany(() => Opinion, {
+    foreignKey: 'opinion_parent',
+  })
+  public opinions: HasMany<typeof Opinion>
+
   @belongsTo(() => User)
   public user: BelongsTo<typeof User>
 
   @belongsTo(() => Affirmation)
   public affirmation: BelongsTo<typeof Affirmation>
-
-  @belongsTo(() => Opinion)
-  public opinion: BelongsTo<typeof Opinion>
 }
