75e6bfcb6ca5814e4acebb59bb5f7f3e651e1d6d
Wiatagan Paz <pazwiatagan@gmail.com>
2022-01-20 15:14:43 -0300

    Migration usermeta criada

---
+++ database/migrations/1642690334705_usermeta.ts
@@ -0,0 +1,34 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+
+export default class Usermeta extends BaseSchema {
+  protected tableName = 'usermeta'
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.
+        bigIncrements('meta_id').
+        unsigned().
+        notNullable()
+      table.
+        bigInteger('user_id').
+        unsigned().
+        notNullable().
+        defaultTo(0).
+        references('id').
+        inTable('users').
+        onDelete('CASCADE')
+      table.string('meta_key').nullable()
+      table.text('meta_value', 'longtext').nullable()
+
+      /**
+       * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
+       */
+      table.timestamp('created_at', { useTz: true })
+      table.timestamp('updated_at', { useTz: true })
+    })
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
