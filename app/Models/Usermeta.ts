1d75773e6f6c405cc9ffb8f47514f585a5192b94
Wiatagan Paz <pazwiatagan@gmail.com>
2022-01-20 15:27:23 -0300

    Model Usermeta criado

---
+++ app/Models/Usermeta.ts
@@ -0,0 +1,22 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column } from '@ioc:Adonis/Lucid/Orm'
+
+export default class Usermeta extends BaseModel {
+  @column({ isPrimary: true })
+  public metaId: number
+
+  @column()
+  public userId: number
+
+  @column()
+  public metaKey: string | null
+
+  @column()
+  public metaValue: string | null
+
+  @column.dateTime({ autoCreate: true })
+  public createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  public updatedAt: DateTime
+}
